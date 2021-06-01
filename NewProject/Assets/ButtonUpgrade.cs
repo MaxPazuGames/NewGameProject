27c4e91ab60dfbb6575a3972afb4fb6ebab85c76
Max Sabirov <maxim-sabirov@rambler.ru>
2021-06-01 22:27:46 +0300

    Upgrades

---
+++ NewProject/Assets/ButtonUpgrade.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonUpgrade : MonoBehaviour
+{
+    public enum Upgrades { upgrade1, upgrade2, upgrade3, upgrade4}
+    public Upgrades upgrades;
+    public int priseToBuy;
+    public Text prise;
+    public Button thisButton;
+
+    private void Start()
+    {
+        UpdatePrise();
+    }
+
+    public void Upgrade()
+    {
+        if (priseToBuy <= UpgraderManager.instance.totalPoint)
+        {
+            UpgraderManager.instance.totalPoint -= priseToBuy;
+            //switch (upgrades)
+            //{
+            //    case Upgrades.upgrade1:
+            //        break;
+            //    case Upgrades.upgrade2:
+            //        break;
+            //    case Upgrades.upgrade3:
+            //        break;
+            //    case Upgrades.upgrade4:
+            //        break;
+            //}
+            Debug.Log(upgrades + " Updated");
+            priseToBuy += 25;
+            UpdatePrise();
+            UpgraderManager.instance.CheckPoints();
+        }
+    }
+
+    void UpdatePrise()
+    {
+        prise.text = "Cost: " + priseToBuy.ToString();
+        UpgraderManager.instance.UpdateTotalPoint();
+    }
+}
---
+++ NewProject/Assets/UpgraderManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgraderManager : MonoBehaviour
+{
+    public static UpgraderManager instance;
+    public ButtonUpgrade[] upgradesButtons;
+    public int totalPoint = 0;
+    [SerializeField] private Text _totalPointsText;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void OnDestroy()
+    {
+        instance = null;
+    } 
+
+    private void Start()
+    {
+        UpdateTotalPoint();
+        CheckPoints();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            totalPoint += 250;
+            UpdateTotalPoint();
+            CheckPoints();
+        }
+    }
+
+    public void CheckPoints()
+    {
+        foreach (ButtonUpgrade button in upgradesButtons)
+        {
+            if (button.priseToBuy > totalPoint)
+            {
+                button.thisButton.interactable = false;
+            }
+            else
+            {
+                button.thisButton.interactable = true;
+            }
+        }
+    }
+
+    public void UpdateTotalPoint()
+    {
+        _totalPointsText.text = "Total Points: " + totalPoint.ToString();
+    }
+}
